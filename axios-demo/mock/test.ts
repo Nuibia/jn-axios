3e9e6b5eab750ca30b561035e5c8485ddfb45ed8
anhf <anhf@xiaoma.cn>
2024-09-24 17:19:07 +0800

    移除`/api/text`的mock POST响应处理
    删除了axios-demo(mock)文件中处理`/api/text`POST请求的mock响应实现。此更改通过移除处理文本响应的代码简化了mock设置。

--- axios-demo/mock/test.ts
+++ axios-demo/mock/test.ts
@@ -47,20 +47,4 @@ export default [
       },
     },
   },
-  {
-    url: '/api/text',
-    method: 'post',
-    rawResponse: async (req, res) => {
-      let reqbody = '';
-      await new Promise(resolve => {
-        req.on('data', chunk => {
-          reqbody += chunk;
-        });
-        req.on('end', () => resolve(undefined));
-      });
-      res.setHeader('Content-Type', 'text/plain');
-      res.statusCode = 200;
-      res.end(`hello, ${reqbody}`);
-    },
-  },
 ] as MockMethod[];
